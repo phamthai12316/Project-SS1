b9e8b732475c1c31b65d1998b88a7c6bda0961cc
phamthai12316 <phamthai12316@gmail.com>
2020-03-05 14:49:50 +0700

    Route Route"

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ app.get("https://harvel-electric.herokuapp.com", function(req, res) {
                 mailchimpConf.login.email,
                 metaResult
               );
-              res.redirect("/?email=" + mailchimpConf.login.email);
+              res.redirect("/category");
             }
           });
       }
