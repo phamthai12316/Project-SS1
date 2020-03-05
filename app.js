553623e8988398613925ae70cb9e7ac115b61244
phamthai12316 <phamthai12316@gmail.com>
2020-03-05 13:44:57 +0700

    Change redirect_uri

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.get("/mailchimp/auth/callback", function(req, res) {
         grant_type: "authorization_code",
         client_id: mailchimpClientId,
         client_secret: mailchimpSecretKey,
-        redirect_uri: "https://harvel-electric.herokuapp.com/",
+        redirect_uri: "http://127.0.0.1:3000/mailchimp/auth/callback",
         code: req.query.code
       })
     )
